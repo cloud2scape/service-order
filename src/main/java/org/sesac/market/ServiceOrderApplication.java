6a6b2b696ba54daad818b12f54deb77168663674
Ok JaeOok <okjaeook98@gmail.com>
2024-10-27 13:44:32 +0900

    init: 프로젝트 생성

---
+++ src/main/java/org/sesac/market/ServiceOrderApplication.java
@@ -0,0 +1,13 @@
+package org.sesac.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceOrderApplication.class, args);
+    }
+
+}
