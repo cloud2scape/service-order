020a30098cf5ba1d83de7ef19c4cc28d52a15503
Ok JaeOok <okjaeook98@gmail.com>
2024-11-02 01:00:45 +0900

    chore: 패키지 구조 변경
    `+` market -> market.{domain}
    `+` hexagonal 아키텍처 패키지 구성
    
    BREAKING-CHANGE: 프로젝트 root 패키지 변경

--- src/main/java/org/sesac/market/order/ServiceOrderApplication.java
+++ src/main/java/org/sesac/market/order/ServiceOrderApplication.java
@@ -1,4 +1,4 @@
-package org.sesac.market;
+package org.sesac.market.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
