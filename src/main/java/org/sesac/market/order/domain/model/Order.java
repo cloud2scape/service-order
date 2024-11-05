768b7288f756edf35125f69a3cb14cf47ee31dda
Ok JaeOok <okjaeook98@gmail.com>
2024-11-05 21:16:01 +0900

    feat: order domain 추가
    pk 값 tsid 반영함

---
+++ src/main/java/org/sesac/market/order/domain/model/Order.java
@@ -0,0 +1,49 @@
+package org.sesac.market.order.domain.model;
+
+import io.hypersistence.utils.hibernate.id.Tsid;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.Comment;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@Getter
+@Builder(toBuilder = true)
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "`order`")
+@Comment("주문")
+public class Order {
+    @Id
+    @Tsid
+    @Comment("ID")
+    private Long id;
+
+    @Comment("상품 ID")
+    private Long productId;
+
+    @Comment("계정 ID")
+    private UUID accountId;
+
+    @Comment("가격")
+    @ColumnDefault("0")
+    private Long price;
+
+    @Comment("수량")
+    @ColumnDefault("1")
+    private Integer quantity;
+
+    @CreatedDate
+    @Column(updatable = false, nullable = false, columnDefinition = "DATETIME DEFAULT CURRENT_TIMESTAMP")
+    @Comment("주문시각")
+    private OffsetDateTime orderDate;
+}
