ebd3a72899b2a7a92af5515da5c8226b651b2acf
laigasus <okjaeook98@gmail.com>
2024-11-21 20:31:23 +0900

    feat: order 주문 command usecase 추가
    `+` 주문 상태 변경

--- src/main/java/org/sesac/market/order/application/port/input/OrderCommand.java
+++ src/main/java/org/sesac/market/order/application/port/input/OrderCommand.java
@@ -2,10 +2,13 @@ package org.sesac.market.order.application.port.input;
 
 import org.sesac.market.order.application.dto.request.CreateOrderRequest;
 import org.sesac.market.order.application.dto.request.DeleteOrderRequest;
+import org.sesac.market.order.application.dto.request.UpdateOrderStateRequest;
 import org.sesac.market.order.domain.model.Order;
 
 public interface OrderCommand {
     Order create(CreateOrderRequest request);
 
     boolean delete(DeleteOrderRequest request);
+
+    Order updateOrderState(UpdateOrderStateRequest request);
 }
